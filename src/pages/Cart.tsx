7595ca8e3ebab61331b510174b72f157c57ebd9c
손소희 <ssh30510044@gmail.com>
2023-01-05 14:20:42 +0900

    useState를 사용하여 Counter 구현

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+interface Istate {
+  counter: number;
+}
+
 function Cart() {
+  const [count, setCount] = useState<Istate>({
+    counter: 0,
+  });
+  const onIncrement = () => {
+    setCount({
+      counter: count.counter + 1,
+    });
+  };
+  const onDecrement = () => {
+    setCount({
+      counter: count.counter - 1,
+    });
+  };
   return (
     <Container>
-      <span>Cart</span>
+      <h2>CART</h2>
+      <CounterBox>
+        <button onClick={onDecrement}>-</button>
+        <span>{count.counter}</span>
+        <button onClick={onIncrement}>+</button>
+      </CounterBox>
     </Container>
   );
 }
@@ -14,4 +36,31 @@ export default Cart;
 const Container = styled.div`
   padding: 30px 10px;
   text-align: center;
+  h2 {
+    font-size: 24px;
+    font-weight: 600;
+  }
+`;
+const CounterBox = styled.div`
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  button {
+    width: 30px;
+    height: 30px;
+    margin: 5px;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  span {
+    background-color: #e5eef3;
+    width: 30px;
+    height: 30px;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
